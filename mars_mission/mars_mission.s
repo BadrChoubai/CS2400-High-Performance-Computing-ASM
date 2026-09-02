b2dba787971b3f5bb23aa7b4789e8681873204f8
Badr Choubai <badrchoubai@outlook.com>
2026-09-02 17:25:06 -0600

    finish project

--- mars_mission/mars_mission.s
+++ mars_mission/mars_mission.s
@@ -1,6 +1,6 @@
     .global _start
 
-@   Define compile time constants to store our safe min and max values
+    @    Define compile time constants to store our safe min and max values
     .equ SAFE_MAX, 30
     .equ SAFE_MIN, -10
 
@@ -13,17 +13,17 @@ _start:
     MOV r2, #0                @ r2 = loop counter
 
 @ Scaffold rest of application data
-    LDR r12, =report        @ r12 = &report
-    LDR r5, [r1]            @ peek at readings[0]
-    MOV r3, #0                @ r3 = Sum
-    MOV r10, r5             @ seed min
-    MOV r11, r5             @ seed max
-    MOV r6, #0              @ positive_count
-    MOV r7, #0              @ negative_count
-    MOV r8, #0              @ unsafe_count
+    LDR    r12, =report        @ r12 = &report
+    LDR    r5, [r1]            @ peek at readings[0]
+    MOV    r3, #0                @ r3 = Sum
+    MOV    r10, r5             @ seed min
+    MOV    r11, r5             @ seed max
+    MOV    r6, #0              @ positive_count
+    MOV    r7, #0              @ negative_count
+    MOV    r8, #0              @ unsafe_count
 
 loop:
-@   have we processed num_readings?
+    @   have we processed num_readings?
     CMP r2, r0
     BGE finished
 
@@ -51,40 +51,65 @@ loop:
     B loop
 
 finished:
-@   Mission One Data -- Sum of all readings, lowest and highest reading
-@   Offset 0, 4, 8
+    @   Mission One Data -- Sum of all readings, lowest and highest reading
+    @   Offset 0, 4, 8
     STR r3, [r12, #0]         @ store sum
     STR r10, [r12, #4]        @ store min
     STR r11, [r12, #8]        @ store max
 
-@   Mission Two Data -- Count positives, negatives, and zeroes
-@   Offset 12, 16, 20
+    @   Mission Two Data -- Count positives, negatives, and zeroes
+    @   Offset 12, 16, 20
     STR r6, [r12, #12]        @ store positive_count
     STR r7, [r12, #16]        @ store negative_count
 
-@   Calculates zero_count for Mission Two
-@   Count of zeroes may be done by taking the difference of positive_count and negative_count  
+    @   Calculates zero_count for Mission Two
+    @   Count of zeroes may be done by taking the difference of positive_count and negative_count
     SUB r9, r0, r6            @ r9 = num_readings - positives
     SUB r9, r9, r7            @ r9 -= negatives
     STR r9, [r12, #20]        @ store zero_count
 
-@   Mission Three Data -- Safety Analysis (How many readings outside of safe range?)
-@   Offset 24
+    @   Mission Three Data -- Safety Analysis (How many readings outside of safe range?)
+    @   Offset 24
     STR r8, [r12, #24]        @ store unsafe_count
 
-@   Mission Four Data
-@   Create a corrected copy of the telemetry in the array labeled `corrected`. 
-@   Clamp each value to the safe range
-
-@   Mission Five Data -- Optional
-@   Offset 28
-
-@   SVC 0
+    @  Mission Four Subroutine
+    BL mission4_clamp         @ jump to routine
 
 stop:
     B stop
 
-    .data 
+mission4_clamp:
+    @   Create a corrected copy of the telemetry in the array labeled `corrected`.
+    @   Clamp each value to the safe range
+    LDR r1, =readings          @ r1 = input pointer -> readings[0]
+    LDR r5, =corrected          @ r5 = output pointer -> corrected[0]
+    MOV r2, #0                   @ reset loop counter (shared with mission4_loop below)
+
+mission4_loop:
+    @   have we processed num_readings?
+    CMP r2, r0
+    BGE mission4_done
+
+    @ load, clamp, store, advance here
+    @ 1. load current reading: r4 = readings[r2]
+    LDR r4, [r1, r2, LSL #2]
+    @ 2. if r4 < SAFE_MIN, clamp r4 up to SAFE_MIN
+    CMP r4, #SAFE_MIN
+    MOVLT r4, #SAFE_MIN
+    @ 3. if r4 > SAFE_MAX, clamp r4 down to SAFE_MAX
+    CMP   r4, #SAFE_MAX
+    MOVGT r4, #SAFE_MAX
+    @ 4. store clamped r4 into corrected[r2]
+    STR r4, [r5, r2, LSL #2]
+    @ 5. increment r2
+    ADD r2, r2, #1
+
+    B mission4_loop
+
+mission4_done:
+    BX lr                        @ return to caller
+
+    .data
 readings:
     .word 12, -5, 0, 27, 31, -18, 7, 42, -2, 15
 num_readings:
@@ -93,3 +118,4 @@ report:
     .space 32
 corrected:
     .space 40
+
