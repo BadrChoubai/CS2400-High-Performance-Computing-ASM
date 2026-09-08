bdab60a7e2ae8b5eb5970f7d9c812d8f47e59da0
Badr Choubai <badrchoubai@outlook.com>
2026-09-08 16:43:57 -0600

    2026-09-08

--- class_3/program.s
+++ class_3/program.s
@@ -2,40 +2,72 @@
 
     .text 
 _start:
-  LDR sp, =stack_top
+    LDR sp, =stack_top
 
-  LDR r0, =readings
-  LDR r1, =num_readings
-  LDR r1, [r1]
-  BL count_thermal_shocks
+    LDR r0, =readings
+    LDR r1, =num_readings
+    LDR r1, [r1]
+    BL  count_thermal_shocks
 
-  LDR r1, =shock_count
-  STR r0, [r1]
+    LDR r1, =shock_count
+    STR r0, [r1]
 
 stop:
-  B stop
+    B stop
 
-@ Inputs: r0 = first integer, r1 = second integer  
-@ Return: r0 = absolute difference
-@ Must Preserve: r4-r6
+@ params    r0 = first integer, r1 = second integer
+@ returns   r0 = absolute_difference 
+absolute_difference:
+    SUBS    r0, r0, r1
+    RSBLT   r0, r0, #0
+    BX      lr
+
+@ params    r0 = array address, r1 = number of readings
+@ returns   r0 = number of thermal shocks
+@ preserves r4-r6 
 count_thermal_shocks:
-  PUSH {r4-r6, pc}
+    PUSH {r4-r6, lr}
 
-  @ Register Plan
-  @ r4 = array pointer
-  @ r5 = adjacent pairs remaining
-  @ r6 = shock count
+    @ Register Plan
+    @ r4 = array pointer
+    @ r5 = adjacent pairs remaining
+    @ r6 = shock count
+    MOV r4, r0
+    MOV r6, #0
+    @ Handle length 0 or 1.
+    CMP r1, #2 
+    BLT return_zero
+    MOV r5, r1
+    SUB r5, r5, #1
 
-  @ TODO: Handle length 0 or 1.
-  @ TODO: Load each adjacent pair.
-  @ TODO: Call absolute_difference
-  @ TODO: Count differences greater than 20
-  @ TODO: Advance and repeat.
-  @ TODO: Move the final count into r0.
+loop:
+    @ Load each adjacent pair.
+    LDR r7, [r4]
+    LDR r8, [r4, #4]
 
-  POP {r4-r6, pc}
+    @ Call absolute_difference(r0, r1)
+    MOV r0, r7
+    MOV r1, r8
+    BL  absolute_difference
 
-    .data
+    @ Count differences greater than 20
+    ADD   r4, r4, #4
+    CMP   r0, #20
+    ADDGT r6, r6, #1
+
+    @ Advance and repeat.
+    SUBS  r5, r5, #1
+    BGT   loop
+
+    @ Move the final count into r0.
+    MOV r0, r6
+    POP {r4-r6, pc}
+
+return_zero:
+    MOV r0, #0
+    POP {r4-r6, pc}
+
+    .data 
 readings:
     .word 12, -5, 0, 27, 31, -18, 7, 42, -2, 15
 num_readings:
@@ -43,7 +75,9 @@ num_readings:
 shock_count:
     .word 0
 
-.balign 8
+    .balign 8
 stack_space:
     .space 1024
 stack_top:
+
+
